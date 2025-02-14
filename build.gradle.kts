3ad6e6b09df6f1bd8bda3179da519ae69927a042
Tomáš Bříza <tomas.briza@gendigital.com>
2025-02-14 12:46:50 +0100

    Disable bootJar task.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,39 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
-	java
-	id("org.springframework.boot") version "3.4.1"
-	id("io.spring.dependency-management") version "1.1.7"
+    java
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.tbr.test"
 version = "0.0.1-SNAPSHOT"
 
+
+// Disable the default bootJar task
+// Since we use only spring boot dependencies management
+tasks.getByName<BootJar>("bootJar") {
+    enabled = false
+}
+
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.slf4j:slf4j-api")
+    implementation("org.slf4j:slf4j-api")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
